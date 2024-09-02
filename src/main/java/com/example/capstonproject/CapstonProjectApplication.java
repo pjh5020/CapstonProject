4c0bff558876289419e0e44d4344e609c5a39f9c
pjh5020 <123628540+pjh5020@users.noreply.github.com>
2024-09-03 00:02:22 +0900

    init

---
+++ src/main/java/com/example/capstonproject/CapstonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.capstonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstonProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstonProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/capstonproject/CapstonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstonproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstonProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
